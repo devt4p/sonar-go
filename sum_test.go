1a7cc4740b8b6b13e8f5527f8a83b3dc4d97a01f
Rodrigo TheCoder <marques.coti@gmail.com>
2022-12-21 22:28:54 -0300

    ci: Add test

--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,27 @@ func TestSum(t *testing.T) {
 		t.Error("The Result must be 5")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(4, 3)
+
+	if result != 1 {
+		t.Error("The Result must be 1")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(4, 3)
+
+	if result != 12 {
+		t.Error("The Result must be 12")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(4, 3)
+
+	if result != 11 {
+		t.Error("The Result must be 11")
+	}
+}
